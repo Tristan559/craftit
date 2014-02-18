e1199e02a26c24e0a65efe31655d4fa458279907
Randy Casey <rcasey559@gmail.com>
2014-02-18 02:08:10 -0800

    Menu Action Bar
    Added menu action bar
    Google icon assets

--- src/com/robotage/crafitit/MainActivity.java
+++ src/com/robotage/crafitit/MainActivity.java
@@ -1,13 +1,15 @@
 package com.robotage.crafitit;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends Activity 
+public class MainActivity extends ActionBarActivity 
 {
 	public final static String EXTRA_MESSAGE = "come.robotage.craftit.MESSAGE";
 	
@@ -20,14 +22,45 @@ public class MainActivity extends Activity
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) 
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     
-    public void sendMessage ( View view )
+    public boolean openSettings()
+	{
+		return false;
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public boolean openSearch()
+	{
+		return false;
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void sendMessage ( View view )
     {
     	Intent intent = new Intent (this, DisplayMessageActivity.class);
     	EditText editText = (EditText)findViewById (R.id.edit_message);
